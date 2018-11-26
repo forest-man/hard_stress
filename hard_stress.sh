2071a70158418997de3500954a787f22cfb3ccab
Mike <mike186@mail.ru>
2018-11-26 14:11:21 +0300

    Common SH test execution file

---
+++ hard_stress.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+function echo_blue {
+    echo -e "\e[34m $1\e[39m"
+}
+
+function echo_red {
+  echo -e "\e[31m[$(get_timestamp)]: $1\e[39m"
+}
+
+function echo_green {
+  echo -e "\e[32m $1\e[39m"
+}
+
+function echo_yellow {
+  echo -e "\e[33m[$(get_timestamp)]: $1\e[39m"
+}
+
+if [ $# != 1 ]
+  then echo_green "Please choose required test: \n1-CPU eat\n2-Memory eat\n3-Diskspace eat"
+  exit;
+fi
+
+function cpu_eat {
+	echo_green "###CPU_EAT###\nPlease select test case:\n1-ONE core 100% CPU consumption\n2-ALL cores 100% CPU consumption(Handle with care)"
+  #taskset -c $1 ./cpu_eat.py
+
+}
+
+
+
+
+
+
+
+
+
+
+
+function main {
+  if [[ "$1" == "1" ]]; then
+    cpu_eat
+   elif [[ "$1" == "2" ]]; then
+      echo "number two"
+   elif [[ "$1" == "3" ]]; then
+      echo "number three"
+
+  fi
+}
+
+
+
+
+
+
+main $1
+
