4ce9e0cc1a40ddf5a553bd429308a0d4f833261c
Mike <mike186@mail.ru>
2018-12-16 23:20:23 +0300

    Added protodame to main script

--- hard_stress.py
+++ hard_stress.py
@@ -2,6 +2,7 @@
 
 import sys
 import time
+import errno
 import argparse
 import subprocess
 from multiprocessing import Pool
@@ -32,19 +33,40 @@ def mem_eat():
         except MemoryError:
             time.sleep(0.01)
 
+# When consuming is started a file named 'd' is created in . directory and started to growing.
+def dame():
+    write_str = "!"*2048*2048*50  # Consume amount
+
+    output_path = sys.argv[1]
+
+    with open(output_path, "w") as f:
+        while True:
+            try:
+                f.write(write_str)
+                f.flush()
+            except IOError as err:
+                if err.errno == errno.ENOSPC:
+                    write_str_len = len(write_str)
+                    if write_str_len > 1:
+                        write_str = write_str[:write_str_len/2]
+                    else:
+                        break
+                else:
+                    raise
+
 
 parser = argparse.ArgumentParser()
 subparsers = parser.add_subparsers()
 
 
-parser_cpu_eat = subparsers.add_parser('c', help='Consume CPU')
-parser_cpu_eat.set_defaults(func=cpu_eat)
+parser_dame = subparsers.add_parser('d', help='Consume Disc space')
+parser_dame.set_defaults(func=dame)
 
 if len(sys.argv) <= 1:
     sys.argv.append('--help')
 
 options = parser.parse_args()
 
-# Run the appropriate function (in this case showtop20 or listapps)
+# Run the appropriate function (in this case dame)
 options.func()
 
