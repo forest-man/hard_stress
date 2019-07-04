133f23211c7eb2472adbc068ca842991ef88514e
Mike <mike186@mail.ru>
2019-07-04 18:58:14 +0300

    Improvements in creation eater# files

--- common_scripts/test_filesize.py
+++ common_scripts/test_filesize.py
@@ -12,31 +12,31 @@ def disc_cons():
     write_str = "Full_space"*20*20*5  # Consume amount
     try:
         print("Discspace consumption is started...\nPlease use \'ctrl+c\' command to exit.")
-        for i in
-        with open('eater', "w") as f:
-            while True:
-                a = os.path.getsize('eater')
-                print(a)
-                try:
-                    idx += 1
-                    if idx > 1:
-                        #if a > 108000000000: for 100gb
-                        if a > 1080000000:
-                            print("Removing 'eater' file...")
-                            #os.remove('eater')
-                            break
-                        idx = 0
-                    f.write(write_str)
-                    f.flush()
-                except IOError as err:
-                    if err.errno == errno.ENOSPC:
-                        write_str_len = len(write_str)
-                        if write_str_len > x:
-                            write_str = write_str[:write_str_len/2]
+        for i in range(1,5):
+            with open('eater' + str(i), "w") as f:
+                while True:
+                    a = os.path.getsize('eater' + str(i))
+                    print(a)
+                    try:
+                        idx += 1
+                        if idx > 1:
+                            #if a > 108000000000: for 100gb
+                            if a > 1080000000:
+                                print("Removing 'eater' file...")
+                                #os.remove('eater')
+                                break
+                                idx = 0
+                                f.write(write_str)
+                                f.flush()
+                    except IOError as err:
+                        if err.errno == errno.ENOSPC:
+                            write_str_len = len(write_str)
+                            if write_str_len > x:
+                                write_str = write_str[:write_str_len/2]
+                            else:
+                                continue
                         else:
-                            continue
-                    else:
-                        break
+                                break
     except (KeyboardInterrupt, OSError):
         os.remove('eater')
         print("")
